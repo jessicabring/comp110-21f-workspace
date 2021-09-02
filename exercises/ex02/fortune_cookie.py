42b3e088b3078fc10ac8ee784a290395f6c9dbbf
Jessica Bring <jbring@ad.unc.edu>
2021-09-02 18:18:14 -0400

    making progress on ex02

--- exercises/ex02/fortune_cookie.py
+++ exercises/ex02/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730394024"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -11,5 +11,18 @@ __author__ = "ENTER YOUR 9-DIGIT PID HERE"
 # It will evaluate to an int value >= 1 and <= 100. 
 from random import randint
 
+message: int = randint(1, 4)
+print("Your fortune cookie says...")
 
-# Begin your solution here...
+if message == 1: 
+    print("You will find success in your academic affairs.")
+else:
+    if message == 2:
+        print("Tomorrow will bring good news.")
+    else:
+        if message == 3: 
+            print("You will recieve communications from your soulmate today.")
+        else: 
+            print("22 will be your lucky number this week.")
+
+print("Now, go spread positive vibes!")
--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -1,6 +1,19 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730394024"
 
 
-# Begin your solution here...
+beat: str = input("What beat do you want to repeat? ")
+number: int = int(input("How many times do you want to repeat it? "))
+counter: int = 1
+music: str = beat
+
+while counter < number:
+    music = music + " " + beat
+    counter = counter + 1
+
+if counter == number:
+    print(music)
+else:
+    if number <= 0:
+        print("No beat...")
