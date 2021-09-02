45164e6dca30b7ca1a94797c1d0f9e4d216c2d76
Jessica Bring <jbring@ad.unc.edu>
2021-09-02 11:47:06 -0400

    practice exercises :)

---
+++ lessons/iterating-through-a-collection.py
@@ -0,0 +1,14 @@
+"""Example of loopign through all characters in a string."""
+
+user_string: str = input("Give me a string! ")
+
+# The variable i is a common counter variable name in programming
+# short for iteration.
+i: int = 0
+
+while i < len(user_string):
+    print(user_string[i])
+    i = i + 1 
+    # so the loop will eventually stop when i=length of string
+    
+print("Done!")
