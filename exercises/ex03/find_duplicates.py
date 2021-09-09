cb69f1be17f5b4ade86f75047cf3b3a7dd323fba
Jessica Bring <jbring@ad.unc.edu>
2021-09-09 19:58:45 -0400

    progress on ex03

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,3 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730394024"
--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,24 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730394024"
 
 
-# Begin your solution here...
+user_word: str
+
+
+def carolina_word(a: int) -> str:
+    """Returns a UNC related phrase depending on the divisibility of the user input by 7 and or 2."""  
+    if a % 2 == 0:
+        user_word = "TAR"
+        if a % 7 == 0:
+            user_word = "TAR HEELS"
+    else: 
+        if a % 7 == 0:
+            user_word = "HEELS"
+        else:
+            user_word = "CAROLINA"
+    return user_word
+
+
+output: str = carolina_word(int(input("Enter an int: ")))
+print(output)
