2a43d4ac5b2483ca03bec36f958a3b1ac2a94993
Jessica Bring <jbring@ad.unc.edu>
2021-09-23 18:13:49 -0400

    List lesson and some of EX04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,41 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730394024"
 
 
 # TODO: Implement your functions here.
+
+def main() -> None:
+    print(all([1, 2, 3], 2))
+    print(is_equal([1, 0, 1], [1, 0, 1]))
+    print(is_equal([1, 1, 0, 4], [1, 0, 1, 4]))
+
+
+def all(a: list[int], b: int) -> bool:
+    i: int = 0
+    while i < len(a):
+        if a[i] == b:
+            return True
+        i += 1
+    return False
+
+
+def is_equal(a: list[int], b: list[int]) -> bool: 
+    i: int = 0
+    while i < len(a):
+        if a[i] == b[i]:
+            i += 1
+        else:
+            return False
+    
+    if i == len(a):
+        return True
+    else: 
+        return False
+    
+
+# max fn 
+
+
+if __name__ == "__main__":
+    main()
