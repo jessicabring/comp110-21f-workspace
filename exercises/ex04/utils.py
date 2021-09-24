fd72b671b5dddfa15f5e99e6e0217ec859c4b48a
Jessica Bring <jbring@ad.unc.edu>
2021-09-24 10:42:29 -0400

    Finished EX04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -5,18 +5,15 @@ __author__ = "730394024"
 
 # TODO: Implement your functions here.
 
-def main() -> None:
-    print(all([1, 2, 3], 2))
-    print(is_equal([1, 0, 1], [1, 0, 1]))
-    print(is_equal([1, 1, 0, 4], [1, 0, 1, 4]))
-
-
 def all(a: list[int], b: int) -> bool:
     i: int = 0
     while i < len(a):
         if a[i] == b:
-            return True
-        i += 1
+            i += 1
+        else: 
+            return False
+    if i == len(a):
+        return True
     return False
 
 
@@ -34,8 +31,13 @@ def is_equal(a: list[int], b: list[int]) -> bool:
         return False
     
 
-# max fn 
-
-
-if __name__ == "__main__":
-    main()
+def max(a: list[int]) -> int:
+    if len(a) == 0:
+        raise ValueError("max() arg is an empty List")
+    i: int = 0
+    list_max: int = a[0]
+    while i < len(a) - 1:
+        if list_max < a[i + 1]:
+            list_max = a[i + 1]
+        i += 1
+    return list_max
