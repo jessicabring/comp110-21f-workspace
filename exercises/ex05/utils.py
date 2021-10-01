c4dca881faee557f048b63ede337b683cc2283e3
Jessica Bring <jbring@ad.unc.edu>
2021-10-01 11:08:01 -0400

    EX05 Progress

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,45 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730394024"
 
-# Define your functions below
+
+def only_evens(a: list[int]) -> list[int]:
+    """Function that takes a list of integers and returns a new list with only even integers."""
+    result: list[int] = list()
+    for x in a:
+        if x % 2 == 0:
+            result.append(x)
+    return result
+
+
+def sub(x: list[int], start: int, end: int) -> list[int]:
+    """Function that takes a list input and bounds and returns a new list with the numbers in between the given bounds."""
+    result: list[int] = list()
+    i: int = 0
+    if end > len(x):
+        end = len(x)
+    if len(x) == 0: 
+        return result
+    else: 
+        if start > len(x):
+            return result
+        else: 
+            if end <= 0:
+                return result
+    while i < end:
+        if i >= start:
+            result.append(x[i])
+        else: 
+            if start < 0:
+                result.append(x[i])
+        i += 1
+    return result
+
+
+def concat(a: list[int], b: list[int]) -> list[int]:
+    result: list[int] = list()
+    for x in a:
+        result.append(x)
+    for y in b: 
+        result.append(y)
+    return result
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -3,4 +3,72 @@
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730394024"
+
+from exercises.ex05.utils import only_evens, sub
+
+
+def test_only_evens_mixed() -> None:
+    """Only evens use case."""
+    xs: list[int] = [1, 2, 3]
+    assert only_evens(xs) == [2]
+
+
+def test_only_evens_lengthy() -> None:
+    """Only evens use case pt 2."""
+    xs: list[int] = [10, 11, 12, 13, 14, 15, 16, 17, 18]
+    assert only_evens(xs) == [10, 12, 14, 16, 18]
+
+
+def test_only_evens_odd_trial() -> None:
+    """Only evens edge case. Should return empty string."""
+    xs: list[int] = [1, 5, 3]
+    assert only_evens(xs) == []
+
+
+def test_only_evens_all_evens() -> None:
+    """Only evens edge case. Should return string back."""
+    xs: list[int] = [4, 4, 4]
+    assert only_evens(xs) == [4, 4, 4]
+
+
+def test_sub_ideal_list() -> None:
+    """Sub use case."""
+    xs: list[int] = [0, 20, 30, 40]
+    assert sub(xs, 1, 3) == [20, 30]
+
+
+def test_sub_use_lengthy() -> None:
+    """Sub use case, longer list"""
+    xs: list[int] = [1, 2, 3, 4, 5, 6, 7]
+    assert sub(xs, 2, 6) == [3, 4, 5, 6]
+
+
+def test_sub_start_is_neg() -> None:
+    """Sub edge case, start is negative."""
+    xs: list[int] = [0, 1, 2, 3, 5]
+    assert sub(xs, -3, 3) == [0, 1, 2]
+
+
+def test_sub_end_is_neg() -> None: 
+    """Sub edge case, end is negative."""
+    xs: list[int] = [0, 1, 2, 3, 5]
+    assert sub(xs, 1, -3) == []
+
+
+def test_sub_end_is_greater_than() -> None:
+    """Sub edge case, should return empty string because end > length."""
+    xs: list[int] = [0, 1, 2, 3, 5]
+    assert sub(xs, 3, 6) == [3, 5]
+
+
+def test_sub_list_is_empty() -> None: 
+    """Sub edge case, should return empty string if input string is empty."""
+    xs: list[int] = list()
+    assert sub(xs, 0, 2) == []
+
+
+def test_sub_start_is_greater() -> None:
+    """Sub edge case, shoudl return empty list because start is > length."""
+    xs: list[int] = [0, 1, 2, 3, 5]
+    assert sub(xs, 7, 3) == []
