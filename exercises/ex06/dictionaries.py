aaf3e9328080dd45a6d3bbbbc5264c8cec5bdfdf
Jessica Bring <jbring@ad.unc.edu>
2021-10-07 16:32:45 -0400

    EX

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,12 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730394024"
 
-# Define your functions below
+# Define your functions below.
+
+
+def invert(map: dict[str, str]) -> dict[str, str]:
+    result: dict[str, str] = dict()
+    for key in map:
+        result[key] = map[key]
+    return result
