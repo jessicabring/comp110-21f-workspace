8c928e4e921144704ed98ff27ce5e62e720e94ce
Jessica Bring <jbring@ad.unc.edu>
2021-10-07 21:31:34 -0400

    Progress on ex06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -6,7 +6,57 @@ __author__ = "730394024"
 
 
 def invert(map: dict[str, str]) -> dict[str, str]:
+    """Takes a dictionary and switches the keys with the values and vice versa."""
     result: dict[str, str] = dict()
     for key in map:
-        result[key] = map[key]
+        if map[key] in result:
+            raise KeyError('KeyError')
+        result[map[key]] = key
     return result
+
+
+def favorite_color(log: dict[str, str]) -> str:
+    """Takes a dictionary and returns the value with highest frequency."""
+    counter: int = 0
+    result: str = "" 
+    first_value: str = ""
+    color_winner: int = 0
+    val_1: str
+    is_duplicate: bool = False
+
+    for key in log:
+        val_1 = log[key]
+        if counter == 0: 
+            first_value = log[key]
+            counter += 1
+        for dup in log:
+            if log[dup] == val_1:
+                counter += 1
+            if counter > 2:    
+                is_duplicate = True
+        if counter == color_winner and is_duplicate is False:
+            result = first_value
+        if counter > color_winner:
+            result = log[key]
+            color_winner = counter
+        
+        counter = 1
+        is_duplicate = False
+    
+    return result
+
+
+def count(a: list[str]) -> dict[str, int]:
+    """Tracks how many times a value is in a list, returns a dictionary with the list values as keys and frequency as values."""
+    i: int = 0
+    key: str
+    result: dict[str, int] = dict()
+    while i < len(a):
+        key = a[i]
+        if key in result:
+            result[key] += 1
+        else:
+            result[key] = 1
+        i += 1
+    return result
+        
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,61 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+from exercises.ex06.dictionaries import invert, favorite_color, count
+
+__author__ = "730394024"
+
+
+def test_invert_duplicate_keys() -> None:
+    """Edge case: tests for duplicate keys."""
+    input: dict[str, str] = {'kris': 'jordan', 'michael': 'jordan'}
+    assert invert(input) == KeyError('KeyError')
+
+
+def test_invert_long() -> None: 
+    """Use case with a longer dictionary input."""
+    input: dict[str, str] = {'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w'}
+    assert invert(input) == {'z': 'a', 'y': 'b', 'x': 'c', 'w': 'd'}
+
+
+def test_invert_normal() -> None:
+    """Use case with average input."""
+    input: dict[str, str] = {'apple': 'cat', 'banana': 'fish', 'peach': 'dog'}
+    assert invert(input) == {'cat': 'apple', 'fish': 'banana', 'dog': 'peach'}
+
+
+def test_favorite_color_tie() -> None:
+    """Edge case, when two colors are tied."""
+    input: dict[str, str] = {"Jessica": "black", "Grace": "blue", "Cece": "red", "Rhiannon": "red", "Rachael": "blue"}
+    assert favorite_color(input) == "black"
+
+
+def test_favorite_color_short() -> None:
+    """Use case, short dictionary input."""
+    input: dict[str, str] = {'Jessica': 'black', 'Michelle': 'black', 'Grace': 'red'}
+    assert favorite_color(input) == "black"
+
+
+def test_favorite_color_long() -> None:
+    """Use case, longer string with many contenders."""
+    input: dict[str, str] = {'Jessica': 'black', 'Michelle': 'red', 'Grace': 'red', 'Rachael': 'red', 'Rhiannon': 'green', 'Cece': 'green', 'Katy': 'black'}
+    assert favorite_color(input) == "red"
+
+
+def test_count_no_duplicates() -> None:
+    """Edge case, no duplicate values in the list input."""
+    input: list[str] = ['the', 'quick', 'brown', 'fox']
+    assert count(input) == {'the': 1, 'quick': 1, 'brown': 1, 'fox': 1}
+
+
+def test_count_long_list() -> None:
+    """Use case with a longer list input."""
+    input: list[str] = ["earth", "wind", "fire", "wind", "fire", "wind"]
+    assert count(input) == {'earth': 1, 'wind': 3, 'fire': 2}
+
+
+def test_count_repeated_list() -> None:
+    """Use case with regular length, all one term."""
+    input: list[str] = ["hi", "hi", "hi", "hi", "hi"]
+    assert count(input) == {'hi': 5}
