c84caa131abb380a33f6fcbeb1fe3e0552e4916d
Jessica Bring <jbring@ad.unc.edu>
2021-10-27 16:54:50 -0400

    EX07

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -32,3 +32,61 @@ def column_values(table: list[dict[str, str]], column: str) -> list[str]:
         item: str = row[column]
         result.append(item) 
     return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oriented table."""
+    result: dict[str, list[str]] = {}
+    
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+        
+    return result
+
+
+def head(columns_data: dict[str, list[str]], i: int) -> dict[str, list[str]]:
+    """Gives the first 'n' row values, where 'n' is given as a parameter."""
+    result: dict[str, list[str]] = {}
+    
+    for row in columns_data:
+        counter: int = 0
+        first_values: list[str] = []
+        while counter < i:
+            first_values.append(columns_data[row][counter])
+            counter += 1
+        result[row] = first_values
+        
+    return result 
+
+
+def select(table: dict[str, list], subset: list[str]) -> dict[str, list[str]]:
+    """Produces a new column based table with a specific subset of the columns."""
+    result: dict[str, list[str]] = {}
+    for column in subset:
+        result[column] = table[column]
+    return result
+
+
+def concat(table_1: dict[str, list[str]], table_2: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Produces a new column based table combining the column based tables given in the parameter."""
+    result: dict[str, list[str]] = {}
+    for column_1 in table_1:
+        result[column_1] = table_1[column_1]
+    for column_2 in table_2:
+        if column_2 in result:
+            result[column_2] += table_2[column_2]
+        else:
+            result[column_2] = table_2[column_2]
+    return result
+
+
+def count(values: list[str]) -> dict[str, int]:
+    """Records the frequency of the """
+    result: dict[str, int] = {}
+    for x in values:
+        if x in result:
+            result[x] += 1
+        else:
+            result[x] = 1
+    return result
