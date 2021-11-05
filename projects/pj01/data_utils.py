d378ff4c008779668416a4a1f3b6ac536a691381
Jessica Bring <jbring@ad.unc.edu>
2021-11-05 16:13:04 -0400

    PJ01

---
+++ projects/pj01/data_utils.py
@@ -0,0 +1,161 @@
+"""Utility functions."""
+
+__author__ = "730394024"
+
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a csv into a 'table'."""
+    result: list[dict[str, str]] = []
+    
+    # Open a handle to the data file
+    file_handle = open(filename, "r", encoding="utf8")
+    
+    # Preapre to read the data file as a CSV rather than just strings
+    csv_reader = DictReader(file_handle)
+
+    # Read each row of the CSV line by line
+    for row in csv_reader:
+        result.append(row)
+
+    # Close the file when we're done, to free its resources
+    file_handle.close()
+    
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item) 
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oriented table."""
+    result: dict[str, list[str]] = {}
+    
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+        
+    return result
+
+
+def head(columns_data: dict[str, list[str]], i: int) -> dict[str, list[str]]:
+    """Gives the first 'n' row values, where 'n' is given as a parameter."""
+    result: dict[str, list[str]] = {}
+    
+    for row in columns_data:
+        counter: int = 0
+        first_values: list[str] = []
+        
+        if i >= len(columns_data[row]):
+            result[row] = columns_data[row]
+        else:
+            while counter < i:
+                first_values.append(columns_data[row][counter])
+                counter += 1
+            result[row] = first_values
+
+    return result 
+
+
+def select(table: dict[str, list[str]], subset: list[str]) -> dict[str, list[str]]:
+    """Produces a new column based table with a specific subset of the columns."""
+    result: dict[str, list[str]] = {}
+    for column in subset:
+        result[column] = table[column]
+    return result
+
+
+def concat(table_1: dict[str, list[str]], table_2: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Produces a new column based table combining the column based tables given in the parameter."""
+    result: dict[str, list[str]] = {}
+    for column_1 in table_1:
+        result[column_1] = table_1[column_1]
+    for column_2 in table_2:
+        if column_2 in result:
+            result[column_2] += table_2[column_2]
+        else:
+            result[column_2] = table_2[column_2]
+    return result
+
+
+def count(values: list[str]) -> dict[str, int]:
+    """Records the frequency of the values in a list."""
+    result: dict[str, int] = {}
+    for x in values:
+        if x in result:
+            result[x] += 1
+        else:
+            result[x] = 1
+    return result
+
+
+def filter(rows: list[dict[str, str]], search: str, search_2: str, data_set: list[str]) -> dict[str, list[str]]:
+    """Searches for up to 2 keyword in the row oriented dataset, then creates a new row-oriented data set with only the rows including the
+    specific keyword(s), converts it back to column based, and selects specific columns to include. It took me forever to get this I'm so happy."""
+
+    dict_convert: list[dict[str, str]] = []
+    result: dict[str, list[str]] = {}
+    
+    for y in rows:
+        for x in y:
+            if y[x] == search:
+                dict_convert.append(y)
+            elif y[x] == search_2:
+                dict_convert.append(y)
+    
+    first_row: dict[str, str] = dict_convert[0]
+    for z in first_row:
+        result[z] = column_values(dict_convert, z) 
+    
+    result = select(result, data_set)
+    
+    return result
+
+
+def frequency_greater(table: dict[str, list[str]], condition: int, numeric_column: str, labels_column: str) -> dict[str, int]:
+    """Counts the frequency of answers being greater than a conditional parameter, converts into dict[str, int]."""
+    result: dict[str, int] = {}
+    int_list: list[int] = []
+    
+    counter: int = 0
+    
+    for x in table[numeric_column]:
+        int_list.append(int(x))
+        
+    while counter < len(int_list):
+        if int_list[counter] > condition:
+            if table[labels_column][counter] in result:
+                result[table[labels_column][counter]] += 1
+            else:
+                result[table[labels_column][counter]] = 1
+        counter += 1
+            
+    return result
+
+
+def frequency_less(table: dict[str, list[str]], condition: int, numeric_column: str, labels_column: str) -> dict[str, int]:
+    """Counts the frequency of answers being less than a conditional parameter, converts into dict[str, int]."""
+    result: dict[str, int] = {}
+    int_list: list[int] = []
+    
+    counter: int = 0
+    
+    for x in table[numeric_column]:
+        int_list.append(int(x))
+        
+    while counter < len(int_list):
+        if int_list[counter] < condition:
+            if table[labels_column][counter] in result:
+                result[table[labels_column][counter]] += 1
+            else:
+                result[table[labels_column][counter]] = 1
+        counter += 1
+            
+    return result
